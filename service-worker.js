e01d91c6cd86d552e451424f82428d5bcb6cdc64
studios-3irmãs <moreiralima848@gmail.com>
2026-08-23 17:38:14 -0300

    Cache availability blocking manager

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='studio-capricho-v15';
-const ASSETS=['./','./index.html','./admin.html','./manifest.json','./config.js','./app.css','./app1.js','./app2.js','./app3.js','./app4.js','./homeimage.js','./reviews.js','./password-reset.js'];
+const CACHE='studio-capricho-v16';
+const ASSETS=['./','./index.html','./admin.html','./manifest.json','./config.js','./app.css','./app1.js','./app2.js','./app3.js','./app4.js','./app5.js','./homeimage.js','./reviews.js','./password-reset.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
